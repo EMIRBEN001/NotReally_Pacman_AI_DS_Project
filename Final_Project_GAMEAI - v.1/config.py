95b6ded982424fd750c11734f1860ae5603b6158
Elijah <114371873+EMIRBEN001@users.noreply.github.com>
2024-11-17 21:16:49 +0700

    Update config.py

--- Final_Project_GAMEAI - v.1/config.py
+++ Final_Project_GAMEAI - v.1/config.py
@@ -1,13 +1,12 @@
 # Constants
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 900
-FPS = 60
-PLAYER_SPEED = 5
+PLAYER_SPEED = 2
+GHOST_SPEED = 4
 PLAYER_LAYER = 4
-ENEMY_LAYER = 3
-BLOCK_LAYER = 3
+ENEMY_LAYER = 4
+BLOCK_LAYER = 5
 GROUND_LAYER = 1
-SCARED_DURATION = 1000
 
 # Colors
 WHITE = (255, 255, 255)
@@ -16,18 +15,19 @@ BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
+DARK_RED = (153, 0, 0)
 
 #Tilemap
-TILESIZE = 30  # Tile size for the map
+TILESIZE = 32  # Tile size for the map
 
 tilemap = [
     "WWWWWWWWWWWWWWWWWWWWW",
     "W........WWW........W",
-    "W.WW.WW..WWW..WW.WW.W",
+    "W.WWWWWW.WWW.WWWWWW.W",
     "W...................W",
     "W.WW.W.WWWWWWW.W.WW.W",
     "W....W....W....W....W",
-    "WWWW.WWW..W..WWW.WWWW",
+    "WWWW.WWWW.W.WWWW.WWWW",
     "WWWW.W         W.WWWW",
     "WWWW.W WWW WWW W.WWWW",
     "TP.... WIR LCW .... T",
@@ -37,7 +37,7 @@ tilemap = [
     "W.........W.........W",
     "W.WW.WWWW.W.WWWW.WW.W",
     "W..W.............W..W",
-    "W..W.W.WWWWWWW.W.W.WW",
+    "WW.W.W.WWWWWWW.W.W.WW",
     "W....W....W....W....W",
     "W.WWWWWWW.W.WWWWWWW.W",
     "W...................W",
